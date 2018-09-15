19223c20c0365ba499357e2d62c50cf2f67e71d0
Luke Bakker <bakker.l@husky.neu.edu>
2018-09-15 14:43:51 -0400

    made a server connection

---
+++ 3/Aux/src/JSONParser.java
@@ -0,0 +1,126 @@
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.CharsetUtil;
+
+public class JSONParser {
+
+  private static ObjectMapper mapper = new ObjectMapper();
+
+  /**
+   * Main method for program. Calls the in/out methods so they read/write to STDIN/OUT
+   * @param args args for method, ignored
+   * @throws Exception if issue with read/write
+   */
+  public static void main(String[] args) throws Exception {
+    startServer();
+
+    //Queue<JsonNode> objects = getJsonNodes(System.in);
+
+    //printJsonNodes(objects, System.out);
+  }
+
+  public static void startServer() throws InterruptedException {
+    EventLoopGroup group = new NioEventLoopGroup();
+
+    try{
+      ServerBootstrap serverBootstrap = new ServerBootstrap();
+      serverBootstrap.group(group);
+      serverBootstrap.channel(NioServerSocketChannel.class);
+      serverBootstrap.localAddress(new InetSocketAddress("localhost", 8000));
+
+      serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+          socketChannel.pipeline().addLast(new JsonServerHandler());
+        }
+      });
+      ChannelFuture channelFuture = serverBootstrap.bind().sync();
+      channelFuture.channel().closeFuture().sync();
+    } catch(Exception e){
+      e.printStackTrace();
+    } finally {
+      group.shutdownGracefully().sync();
+    }
+
+  }
+
+  /**
+   * Adds the given JsonNodes as a String to the given Appendable
+   *
+   * @param objects JsonNodes to add to Appendable
+   * @param app Appendable to add JsonNodes to
+   * @throws IOException if something goes wrong appending the Strings
+   */
+  public static void printJsonNodes(Queue<JsonNode> objects, Appendable app) throws IOException {
+    int size = objects.size();
+    for (int i = size - 1; i >= 0; i -= 1) {
+      String s = String.format("[%d,%s]\n", i, objects.poll());
+      app.append(s);
+    }
+  }
+
+  /**
+   * Interprets the InputStream to a queue of JsonNodes
+   *
+   * @param stream InputStream to interpret
+   * @return Queue of JsonNodes represented by the InputStream
+   * @throws IOException if something goes wrong processing the input stream, eg, invalid input
+   */
+  public static Queue<JsonNode> getJsonNodes(InputStream stream) throws IOException {
+    Queue<JsonNode> objects = new LinkedList<>();
+    JsonParser parser = new JsonFactory().createParser(stream);
+
+    while (!parser.isClosed()) {
+      JsonNode node = mapper.readTree(parser);
+
+      if (node != null) {
+        objects.offer(node);
+      }
+    }
+
+    return objects;
+  }
+}
+
+class JsonServerHandler extends ChannelInboundHandlerAdapter {
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    ByteBuf inBuffer = (ByteBuf) msg;
+
+    ByteBufInputStream bufInStream = new ByteBufInputStream(inBuffer);
+    Queue<JsonNode> nodes = JSONParser.getJsonNodes(bufInStream);
+    JSONParser.printJsonNodes(nodes, System.out);
+  }
+
+  @Override
+  public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+    ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+            .addListener(ChannelFutureListener.CLOSE);
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
---
+++ 3/Aux/test/JSONParserTest.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.assertEquals;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Queue;
+import org.junit.Test;
+
+public class JSONParserTest {
+
+  /*
+  Tests that the parser works with the given example on the assignment
+   */
+  @Test
+  public void testBasicInput() throws IOException {
+    byte[] data = "[1,2]\n{\"a\":1}".getBytes();
+
+    InputStream input = new ByteArrayInputStream(data);
+
+    Queue<JsonNode> nodes = JSONParser.getJsonNodes(input);
+
+    StringBuilder output = new StringBuilder();
+    JSONParser.printJsonNodes(nodes, output);
+
+    assertEquals("[1,[1,2]]\n[0,{\"a\":1}]\n", output.toString());
+  }
+
+  /*
+  Tests that the parser ignores any blank lines in the input
+   */
+  @Test
+  public void testBlankLines() throws IOException {
+    byte[] data = "[\n1\n\n   ]".getBytes();
+    InputStream input = new ByteArrayInputStream(data);
+
+    Queue<JsonNode> nodes = JSONParser.getJsonNodes(input);
+
+    StringBuilder output = new StringBuilder();
+    JSONParser.printJsonNodes(nodes, output);
+
+    assertEquals("[0,[1]]\n", output.toString());
+  }
+
+  /*
+  Tests that the parser does not print anything when given no input
+   */
+  @Test
+  public void testEmptyInput() throws IOException {
+    byte[] data = "".getBytes();
+    InputStream input = new ByteArrayInputStream(data);
+
+    Queue<JsonNode> nodes = JSONParser.getJsonNodes(input);
+
+    StringBuilder output = new StringBuilder();
+    JSONParser.printJsonNodes(nodes, output);
+
+    assertEquals("", output.toString());
+  }
+
+  /*
+  Tests that the parser throws an exception if given input that is not valid JSON
+   */
+  @Test(expected = com.fasterxml.jackson.core.JsonParseException.class)
+  public void testException() throws IOException {
+    byte[] data = ",".getBytes();
+    InputStream input = new ByteArrayInputStream(data);
+
+    JSONParser.getJsonNodes(input);
+  }
+}
