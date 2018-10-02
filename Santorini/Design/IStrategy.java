052b7db385a84fdea3982b01dee20f925fd2e5f7
Luke Bakker <bakker.l@husky.neu.edu>
2018-10-02 15:34:01 -0400

    added strategy interface

---
+++ Santorini/Design/IStrategy.java
@@ -0,0 +1,54 @@
+/**
+ * Interface for the strategy class. Makes decisions about player moves
+ * based on the current state of the board.
+ */
+public interface IStrategy {
+  // The strategy knows the dimensions of the board
+  // The strategy knows the target height to win the game
+  // The strategy knows the maximum height for a building.
+
+  /**
+   * determines where to place a new worker based on the current state
+   * of the board.
+   * @param board The current state of the board. This should be a copy.
+   * @return Posn to place the new worker at a valid location on the board.
+   */
+  Posn placeWorker(IBoard board);
+
+
+  /**
+   * Determines which worker to move, where to move them, and where to build (if not at
+   * target height already).
+   * @param board The current state of the board.
+   * @return an instance of the Move class.
+   */
+  Move makeMove(IBoard board);
+
+
+}
+
+/**
+ * This is a data definition for a move. A move always has a worker. A move occurs in
+ * two steps. First the given worker moves int moveDx and int moveDy [-1,1]. Then
+ * from THAT location the worker may build to an adjacent building specified by buildDx
+ * and buildDy [-1,1].
+ */
+class Move {
+
+  Worker worker;
+  int moveDx;
+  int moveDy;
+  int buildDx;
+  int buildDy;
+
+}
+
+
+/**
+ * This is a data definition for a Posn. A posn has an int x and an int y. These should
+ * correspond to buildings on the board.
+ */
+class Posn {
+  int x;
+  int y;
+}
