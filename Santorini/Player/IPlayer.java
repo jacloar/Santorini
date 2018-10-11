0377165d6c7f57327eb234fbe326dc088d5e0e3d
Luke Bakker <bakker.l@husky.neu.edu>
2018-10-11 17:55:48 -0400

    Iplayer

--- Santorini/Player/IPlayer.java
+++ Santorini/Player/IPlayer.java
@@ -1,6 +1,7 @@
 package Player;
 
-import Common.IBoard;
+import java.util.List;
+
 import Common.Posn;
 import Common.Worker;
 
@@ -23,9 +24,11 @@ public interface IPlayer {
    * During the startup phase of the game the admin will tell the player
    * it is their turn to place a worker on the board.
    *
+   * @param heights the 2D array of heights representing the board
+   * @param workerPosns the list of positions on the board where workers are
    * @return posn where the strategy requested to place a worker
    */
-  Posn placeWorker();
+  Posn placeWorker(int[][] heights, List<Posn> workerPosns);
 
 
   /**
@@ -34,9 +37,16 @@ public interface IPlayer {
    * direction, and then build on to an adjacent building. By the end
    * of this method the player's turn will be complete.
    *
+   * @param heights the 2D array of heights representing the board
+   * @param workerPosns the list of positions on the board where workers are
    * @return a move request
    */
-  Move completeTurn();
+  Move completeTurn(int[][] heights, List<Posn> workerPosns);
 
+  /**
+   * Simply returns the name of the player.
+   * @return player name
+   */
+  String getName();
 }
 
