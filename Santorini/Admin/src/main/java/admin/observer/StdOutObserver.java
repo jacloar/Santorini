167336aa1e22aa71c31e4df71d0bc3d25075b58a
Jack Loar <loar.jo@husky.neu.edu>
2018-10-24 17:54:45 -0400

    Completed printBoard

--- Santorini/Admin/src/main/java/admin/observer/StdOutObserver.java
+++ Santorini/Admin/src/main/java/admin/observer/StdOutObserver.java
@@ -2,15 +2,30 @@ package admin.observer;
 
 import common.board.IReadonlyBoard;
 import common.data.Action;
+import java.io.IOException;
 import java.util.List;
+import java.util.function.Function;
 import player.IPlayer;
 
 public class StdOutObserver implements IObserver {
 
-  
+  private final Appendable app;
+
+  public StdOutObserver() {
+    this(System.out);
+  }
+
+  public StdOutObserver(Appendable app) {
+    this.app = app;
+  }
+
   @Override
   public void update(IReadonlyBoard board) {
-
+    try {
+      printBoard(board);
+    } catch (IOException e) {
+      throw new RuntimeException("Error printing board", e);
+    }
   }
 
   @Override
@@ -31,5 +46,42 @@ public class StdOutObserver implements IObserver {
   @Override
   public void updateError(String error) {
 
+  }
+
+  /**
+   * Prints the board to Appendable.
+   *
+   * @param board board to print
+   * @throws IOException if something goes wrong with IO
+   */
+  private void printBoard(IReadonlyBoard board) throws IOException {
+    app.append("[");
+
+    for (int row = 0; row < board.getMaxRows(); row += 1) {
+      app.append("[");
+
+      for (int col = 0; col < board.getMaxColumns(); col += 1) {
+        board.getCell(row, col).appendSelf(app);
+        appendCommaOrBrace(board, col, IReadonlyBoard::getMaxColumns);
+      }
+
+      appendCommaOrBrace(board, row, IReadonlyBoard::getMaxRows);
+    }
+  }
+
+  /**
+   * Prints either a comma or brace to Appendable based on the given number and the getMax function
+   *
+   * @param board board we are printing
+   * @param index current index
+   * @param getMax function that returns the max index when supplied with board
+   * @throws IOException if something goes wrong with IO
+   */
+  private void appendCommaOrBrace(IReadonlyBoard board, int index, Function<IReadonlyBoard, Integer> getMax) throws IOException {
+    if (index + 1 < getMax.apply(board)) {
+      app.append(",");
+    } else {
+      app.append("]");
+    }
   }
 }
