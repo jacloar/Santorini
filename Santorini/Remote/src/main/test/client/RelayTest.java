c466524bdec670724b3d56c4a55a03d79173e65e
Jack Loar <loar.jo@husky.neu.edu>
2018-11-23 14:44:28 -0500

    Wrote test for take turn

--- Santorini/Remote/src/main/test/client/RelayTest.java
+++ Santorini/Remote/src/main/test/client/RelayTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import common.board.Board;
+import common.board.IBoard;
 import common.interfaces.IPlayer;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +19,7 @@ import strategy.Strategy;
 
 public class RelayTest {
 
-  private static String name = "name";
+  private static String name = "one";
   private static ObjectMapper mapper = new ObjectMapper();
 
   private Relay relay;
@@ -34,6 +36,9 @@ public class RelayTest {
     relay = new Relay(player);
   }
 
+  /**
+   * Tests that a playing as message will update the name of a player
+   */
   @Test
   public void testPlayingAs() {
     String playingAs = "playing-as";
@@ -50,6 +55,9 @@ public class RelayTest {
     assertThat(player.getPlayerName()).isEqualTo(newName);
   }
 
+  /**
+   * Tests that an empty placement will get back a valid place
+   */
   @Test
   public void testEmptyPlacement() {
     ArrayNode placementMessage = mapper.createArrayNode();
@@ -66,8 +74,11 @@ public class RelayTest {
     assertThat(place.get(1).asInt()).isEqualTo(0);
   }
 
+  /**
+   * Tests that a non empty placement will get back a valid place
+   */
   @Test
-  public void testNonEmptyWorkerPlace() {
+  public void testNonEmptyPlacement() {
     String otherWorker = "other1";
     int otherX = 0;
     int otherY = 0;
@@ -92,9 +103,32 @@ public class RelayTest {
     assertThat(place.get(1).asInt()).isEqualTo(1);
   }
 
+  /**
+   * Tests that a take turn message will result in an appropriate action
+   */
   @Test
   public void testTakeTurn() {
+    String player1 = name;
+    String player2 = "two";
 
+    IBoard board = new Board();
+
+    board.createWorker(player1, 0, 0);
+    board.createWorker(player2, 1, 1);
+    board.createWorker(player1, 2, 2);
+    board.createWorker(player2, 3, 3);
+
+    player.setOpponentName(player2);
+
+    JsonNode action = relay.respond(board.toJson()).get();
+
+    assertThat(action.isArray()).isTrue();
+    assertThat(action).hasSize(5);
+    assertThat(action.get(0).asText()).isEqualTo("one1");
+    assertThat(action.get(1).asText()).isEqualTo("EAST");
+    assertThat(action.get(2).asText()).isEqualTo("PUT");
+    assertThat(action.get(3).asText()).isEqualTo("EAST");
+    assertThat(action.get(4).asText()).isEqualTo("PUT");
   }
 
 }
