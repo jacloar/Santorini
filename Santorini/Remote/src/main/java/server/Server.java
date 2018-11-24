f8f64e37929d745d58e14317754c6f2786bfb915
Jack Loar <loar.jo@husky.neu.edu>
2018-11-24 18:10:06 -0500

    Updated server to accept multiple connections

--- Santorini/Remote/src/main/java/server/Server.java
+++ Santorini/Remote/src/main/java/server/Server.java
@@ -1,22 +1,59 @@
 package server;
 
 import common.interfaces.IPlayer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class StartServer {
+public class Server {
+
+  Reader reader;
+
+  public Server() {
+    this(new BufferedReader(new InputStreamReader(System.in)));
+  }
+
+  public Server(Reader reader) {
+    this.reader = reader;
+  }
 
   public static void main(String[] args) throws Exception {
     startServer();
   }
 
-  private static void startServer() throws Exception {
+  public static void startServer() throws Exception {
+    int minPlayers = 3;
     int portNumber = 8000;
-    ServerSocket serverSocket = new ServerSocket(portNumber);
-    Socket socket = serverSocket.accept();
+    int waitingFor = 10;
+    int repeat = 0;
 
-    IPlayer player = new RemotePlayer(socket);
-    Thread.sleep(10000);
-    System.out.println(player.getPlayerName());
+    ServerSocket serverSocket = new ServerSocket(portNumber);
+    List<Socket> connections = new ArrayList<>();
+    long endTime = System.currentTimeMillis() + (waitingFor * 1000);
+
+    System.out.println("accepting connections");
+    while (System.currentTimeMillis() <= endTime) {
+      System.out.println(System.currentTimeMillis());
+      Socket connection = serverSocket.accept();
+      connections.add(connection);
+    }
+
+    System.out.println("creating remote players");
+    List<IPlayer> players = new ArrayList<>();
+    for (Socket s : connections) {
+      players.add(new RemotePlayer(s));
+    }
+
+    System.out.println("printing names");
+    System.out.println(players.stream().map(IPlayer::getPlayerName).collect(Collectors.toList()));
+
+    for (Socket s : connections) {
+      s.close();
+    }
   }
 }
