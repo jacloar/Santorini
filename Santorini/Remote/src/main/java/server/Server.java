13c46c0d49d750cab29a7b60e8ae75f7a77165b5
Jack Loar <loar.jo@husky.neu.edu>
2018-11-25 14:17:04 -0500

    Server stops connections after time limit and informs players of results

--- Santorini/Remote/src/main/java/server/Server.java
+++ Santorini/Remote/src/main/java/server/Server.java
@@ -1,14 +1,16 @@
 package server;
 
-import common.interfaces.IPlayer;
+import admin.result.GameResult;
+import admin.tournament.ITournamentManager;
+import admin.tournament.TournamentManager;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Server {
 
@@ -38,19 +40,32 @@ public class Server {
 
     System.out.println("accepting connections");
     while (System.currentTimeMillis() <= endTime) {
-      System.out.println(System.currentTimeMillis());
-      Socket connection = serverSocket.accept();
-      connections.add(connection);
+      serverSocket.setSoTimeout((int) (endTime - System.currentTimeMillis()));
+      try {
+        Socket connection = serverSocket.accept();
+        connections.add(connection);
+      } catch (SocketTimeoutException e) {
+        // do nothing, want to continue
+      }
     }
 
-    System.out.println("creating remote players");
-    List<IPlayer> players = new ArrayList<>();
-    for (Socket s : connections) {
-      players.add(new RemotePlayer(s));
-    }
+    // Run the tournament if enough players connected.
+    if (connections.size() >= minPlayers) {
+      System.out.println("creating remote players");
+      List<RemotePlayer> players = new ArrayList<>();
+      for (Socket s : connections) {
+        players.add(new RemotePlayer(s));
+      }
 
-    System.out.println("printing names");
-    System.out.println(players.stream().map(IPlayer::getPlayerName).collect(Collectors.toList()));
+      ITournamentManager tm = new TournamentManager();
+      tm.runTournament(players);
+
+      // A tournament has been run, so results is not empty
+      List<GameResult> results = tm.getResults().get();
+      for (RemotePlayer player : players) {
+        player.inform(results);
+      }
+    }
 
     for (Socket s : connections) {
       s.close();
